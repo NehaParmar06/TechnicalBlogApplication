4aea1480c78935811576972c95800c495aa5f248
NehaParmar06 <neha.parmar06@gmail.com>
2019-05-18 13:50:55 +0530

    First Milestone

---
+++ src/main/java/Upgrad/Model/Post.java
@@ -0,0 +1,34 @@
+package Upgrad.Model;
+
+import java.util.Date;
+
+public class Post {
+
+    public String title;
+    public Date date;
+    public String body;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
